7f81bd336bb876ce765565bc3ad97609711623d7
git-hkj <76410061+git-hkj@users.noreply.github.com>
2023-02-28 11:51:46 +0100

    Keeping Waypoint

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [Tooltip("Define the grid size")][SerializeField] Vector2Int gridSize;
+    [Tooltip("Define the grid size")]
+    [SerializeField] Vector2Int gridSize;
+    [Tooltip("Size of the continent: Should match Unity Editor snap settings")]
+    [SerializeField] int unityGridSize = 10;
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
-    
+
+    //Property definition 
+    public int UnityGridSize { get { return UnityGridSize; } }
+
     //Property for accessing
     public Dictionary<Vector2Int,Node> Grid { get { return grid; } }
 
@@ -36,5 +42,35 @@ public class GridManager : MonoBehaviour
                 grid.Add(coordinates,new Node(coordinates, true));
                 Debug.Log(grid[coordinates].coordinates + " = " + grid[coordinates].isWalkable);
             }
+    }
+
+    //To check where enemy can walk
+    public void BlockedNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable = false;
+        }
+
+    }
+
+    //To get the coodinates of the position
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / unityGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / unityGridSize);
+
+        return coordinates;
+    }
+
+    //To get the position from the coordinates
+    public Vector3 GetPositionFromCoordinates(Vector2Int coordinates)
+    {
+        Vector3 position = new Vector3();
+        position.x = coordinates.x * unityGridSize;
+        position.z = coordinates.y * unityGridSize;
+
+        return position;
     }
 }
--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -79,8 +79,9 @@ public class CoordinateLabeler : MonoBehaviour
     //to display the coordinate of the tiles
     void DisplayCoordinate()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        if (gridManager == null) { return; }
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / gridManager.UnityGridSize);
         label.text = coordinates.x + "," + coordinates.y;
     }
 
